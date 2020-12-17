8737f97db76e2a338026fd74b11e64a852750454
antiyro <bouhadananathan@gmail.com>
2020-12-17 18:24:36 +0100

    header modif

--- get_next_line.h
+++ get_next_line.h
@@ -13,8 +13,6 @@
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-# include <unistd.h>
-# include <stdio.h>
 # include <stdlib.h>
 # include <sys/types.h>
 # include <sys/stat.h>
@@ -25,16 +23,12 @@
 # endif
 
 int		get_next_line(int fd, char **line);
-char	*ft_strndup(const char *s, size_t n);
+char	*read_line(char *str, int fd, int *r);
+char	*get_line(char *str, char **line, int r);
 char	*ft_strdup(const char *s);
 size_t	ft_strlen(const char *str);
 char	*ft_strjoin(char const *s1, char const *s2);
-size_t	ft_strlcat(char *dst, const char *src, size_t size);
-size_t	ft_strcpy(char *dst, const char *src);
-void	*ft_memset(void *s, int c, size_t n);
-char	*ft_strnew(size_t size);
 char	*ft_strchr(const char *s, int c);
-void	ft_bzero(void *s, size_t n);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 
 #endif
